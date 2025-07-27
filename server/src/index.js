7eab0cf9dec8ebab7f474c698f79848a4f2ad51f
Keerthinarayan25 <keerthinarayan10@gmail.com>
2025-07-27 20:14:06 +0530

    "project completed"

--- server/src/index.js
+++ server/src/index.js
@@ -31,7 +31,7 @@ app.use("/api/message",messageRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../client/dist")));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../../client", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../../client", "index.html"));
   });
 }
 
