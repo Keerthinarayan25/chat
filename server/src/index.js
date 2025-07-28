812440e72ab320a951b4ca6e205bd2c94760d956
Keerthinarayan25 <keerthinarayan10@gmail.com>
2025-07-29 00:11:53 +0530

    fixed PORT and file path

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {PORT} from '../config/env.js';
+
 import userRouter from '../routes/user.routes.js';
 import messageRouter from '../routes/message.Routes.js';
 import connectToDatabase from '../database/mongodb.js';
@@ -9,7 +9,7 @@ import cookieParser from "cookie-parser";
 import { app, server } from "../utils/socket.js"
 import path from "path";
 
-
+const PORT = process.env.PORT || 10000;
 
 dotenv.config();
 
@@ -30,8 +30,8 @@ app.use("/api/message",messageRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../client/dist")));
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../../client", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../../client", "dist", "index.html"));
   });
 }
 
