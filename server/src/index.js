97d946a38534561453c2d85f72ff0e57d7a4ef38
Keerthinarayan25 <keerthinarayan10@gmail.com>
2026-07-13 19:11:54 +0530

    fixed index.js

--- server/src/index.js
+++ server/src/index.js
@@ -9,19 +9,19 @@ import cookieParser from "cookie-parser";
 import { app, server } from "../utils/socket.js"
 import path from "path";
 
-const PORT = process.env.PORT || 10000;
-
 dotenv.config();
+const PORT = process.env.PORT || 10000;
+const __dirname = path.resolve();
+
+app.use(express.json())
+app.use(cookieParser());
 
 app.use(cors({
   origin:'http://localhost:5173',
   credentials: true,
 }))
 
-const __dirname = path.resolve();
 
-app.use(express.json())
-app.use(cookieParser());
 
 
 app.use("/api/auth", userRouter);
@@ -30,7 +30,7 @@ app.use("/api/message",messageRouter);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../client/dist")));
-  app.get("*", (req, res) => {
+  app.get("/*splat", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../../client", "dist", "index.html"));
   });
 }
